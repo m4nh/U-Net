f65e84b4fab6c1cb82486ab78b94850a99ef33fe
pierlui92 <zamapierluigi@hotmail.it>
2017-11-07 22:07:16 +0100

    init

---
+++ main.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+import scipy.misc
+import numpy as np
+import tensorflow as tf
+from model import U_Net
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--dataset_dir', dest='dataset_dir', default='horse2zebra', help='path of the dataset')
+parser.add_argument('--epoch', dest='epoch', type=int, default=200, help='# of epoch')
+parser.add_argument('--batch_size', dest='batch_size', type=int, default=1, help='# images in batch')
+parser.add_argument('--load_size', dest='load_size', type=int, default=286, help='scale images to this size')
+parser.add_argument('--fine_size', dest='fine_size', type=int, default=256, help='then crop to this size')
+parser.add_argument('--num_sample',dest='num_sample',type=int,default=330,help='number of sample to process for 1 epoch')
+parser.add_argument('--input_nc', dest='input_nc', type=int, default=3, help='# of input image channels')
+parser.add_argument('--output_nc', dest='output_nc', type=int, default=3, help='# of output image channels')
+parser.add_argument('--niter', dest='niter', type=int, default=200, help='# of iter at starting learning rate')
+parser.add_argument('--lr', dest='lr', type=float, default=0.0002, help='initial learning rate for adam')
+parser.add_argument('--beta1', dest='beta1', type=float, default=0.5, help='momentum term of adam')
+parser.add_argument('--flip', dest='flip', type=bool, default=True, help='if flip the images for data argumentation')
+parser.add_argument('--phase', dest='phase', help='train, test', required=True, choices=['train','test'])
+parser.add_argument('--checkpoint_dir', dest='checkpoint_dir', default='./checkpoint', help='models are saved here')
+parser.add_argument('--test_dir', dest='test_dir', default='./test', help='test sample are saved here')
+parser.add_argument('--ngf', dest='ngf', type=int, default=32, help='# of gen filters in first conv layer')
+parser.add_argument('--ndf', dest='ndf', type=int, default=64, help='# of discri filters in first conv layer')
+args = parser.parse_args()
+
+def main(_):
+    if not os.path.exists(args.checkpoint_dir):
+        os.makedirs(args.checkpoint_dir)
+
+    with tf.Session() as sess:
+        model = U_Net(sess, args)
+        if args.phase == 'train':
+            model.train(args)
+        else: 
+            model.test(args)
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ module.py
@@ -0,0 +1,68 @@
+from __future__ import division
+import tensorflow as tf
+from ops import *
+
+def u_net_model(image, options, reuse=False, name="U-Net"):
+
+    with tf.variable_scope(name):
+        # image is 256 x 256 x input_c_dim
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+        else:
+            assert tf.get_variable_scope().reuse == False
+
+        # image is (256 x 256 x input_c_dim)
+        e1 = conv2d(image, options.gf_dim, name='g_e1_conv')
+        # e1 is (128 x 128 x self.gf_dim)
+        e2 = instance_norm(conv2d(lrelu(e1), options.gf_dim*2, name='g_e2_conv'), 'g_bn_e2')
+        # e2 is (64 x 64 x self.gf_dim*2)
+        e3 = instance_norm(conv2d(lrelu(e2), options.gf_dim*4, name='g_e3_conv'), 'g_bn_e3')
+        # e3 is (32 x 32 x self.gf_dim*4)
+
+        e4 = instance_norm(conv2d(lrelu(e3), options.gf_dim*8, name='g_e4_conv'), 'g_bn_e4')
+        # e4 is (16 x 16 x self.gf_dim*8)
+        e5 = instance_norm(conv2d(lrelu(e4), options.gf_dim*8, name='g_e5_conv'), 'g_bn_e5')
+        # e5 is (8 x 8 x self.gf_dim*8)
+        e6 = instance_norm(conv2d(lrelu(e5), options.gf_dim*8, name='g_e6_conv'), 'g_bn_e6')
+        # e6 is (4 x 4 x self.gf_dim*8)
+        e7 = instance_norm(conv2d(lrelu(e6), options.gf_dim*8, name='g_e7_conv'), 'g_bn_e7')
+        # e7 is (2 x 2 x self.gf_dim*8)
+        e8 = instance_norm(conv2d(lrelu(e7), options.gf_dim*8, name='g_e8_conv'), 'g_bn_e8')
+        # e8 is (1 x 1 x self.gf_dim*8)
+
+        d1 = deconv2d(tf.nn.relu(e8), options.gf_dim*8, name='g_d1')
+        d1 = tf.concat([tf.nn.dropout(instance_norm(d1, 'g_bn_d1'), 0.5), e7], 3)
+        # d1 is (2 x 2 x self.gf_dim*8*2)
+
+        d2 = deconv2d(tf.nn.relu(d1), options.gf_dim*8, name='g_d2')
+        d2 = tf.concat([tf.nn.dropout(instance_norm(d2, 'g_bn_d2'), 0.5), e6], 3)
+        # d2 is (4 x 4 x self.gf_dim*8*2)
+
+        d3 = deconv2d(tf.nn.relu(d2), options.gf_dim*8, name='g_d3')
+        d3 = tf.concat([tf.nn.dropout(instance_norm(d3, 'g_bn_d3'), 0.5), e5], 3)
+        # d3 is (8 x 8 x self.gf_dim*8*2)
+
+        d4 = deconv2d(tf.nn.relu(d3), options.gf_dim*8, name='g_d4')
+        d4 = tf.concat([instance_norm(d4, 'g_bn_d4'), e4], 3)
+        # d4 is (16 x 16 x self.gf_dim*8*2)
+
+        d5 = deconv2d(tf.nn.relu(d4), options.gf_dim*4, name='g_d5')
+        d5 = tf.concat([instance_norm(d5, 'g_bn_d5'), e3], 3)
+        # d5 is (32 x 32 x self.gf_dim*4*2)
+
+        d6 = deconv2d(tf.nn.relu(d5), options.gf_dim*2, name='g_d6')
+        d6 = tf.concat([instance_norm(d6, 'g_bn_d6'), e2], 3)
+        # d6 is (64 x 64 x self.gf_dim*2*2)
+
+        d7 = deconv2d(tf.nn.relu(d6), options.gf_dim, name='g_d7')
+        d7 = tf.concat([instance_norm(d7, 'g_bn_d7'), e1], 3)
+        # d7 is (128 x 128 x self.gf_dim*1*2)
+
+        d8 = deconv2d(tf.nn.relu(d7), options.output_c_dim, name='g_d8')
+        # d8 is (256 x 256 x output_c_dim)
+
+        return tf.nn.tanh(d8)
+
+def sem_criterion(logits,labels):
+    labels = tf.cast(labels,tf.int32)
+    return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=tf.squeeze(labels, axis=3)))
