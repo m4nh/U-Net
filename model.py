e7eb851910f04fad6a2b8b7a296b0844aef2ba74
pierlui92 <zamapierluigi@hotmail.it>
2018-04-03 21:37:10 +0200

    update

--- model.py
+++ model.py
@@ -67,21 +67,20 @@ class U_Net(object):
             image_sem = tf.read_file(image_tensor[1])
             image = tf.image.decode_image(image, channels = 3)
             image_sem = tf.image.decode_image( image_sem , channels = 1)
-            image.set_shape([None, None, self.input_c_dim])
-            image_sem.set_shape([None, None,1])
             return image , image_tensor[0], image_sem
         
         samples=[]
         samples_sem = []
+        
         with open(input_list_txt) as input_list:
             for line in input_list:
                 sample,sample_sem = line.strip().split(";")
                 samples.append(sample)
                 samples_sem.append(sample_sem)
-
+        print(samples[0],samples_sem[0])
         image_tensor = tf.constant(np.stack((samples, samples_sem), axis = -1))
 
-        dataset = tf.contrib.data.Dataset.from_tensor_slices(image_tensor)
+        dataset = tf.data.Dataset.from_tensor_slices(image_tensor)
         dataset = dataset.map(_parse_function)
         dataset = dataset.repeat()
         iterator = dataset.make_one_shot_iterator()
